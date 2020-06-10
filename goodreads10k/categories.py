97134b957b346a78ae81f685d00d9aa9b441ee38
Satyrr <gruzi12@gmail.com>
2020-06-10 18:38:34 +0200

    Categories script update

--- goodreads10k/categories.py
+++ goodreads10k/categories.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-category_tags = [
+_category_tags = [
     #'Art',
     #'Biography'
     #'Business',
@@ -8,41 +8,43 @@ category_tags = [
     "Children-s",
     #'Christian',
     'Classics',
-    'Comics',
-    'Contemporary',
-    'Cookbooks',
-    'Crime',
+    #'Comics',
+    #'Contemporary',
+    #'Cookbooks',
+    #'Crime',
     'Fantasy',
-    'Fiction',
+    #'Fiction',
     #'Graphic-Novels',
     #'Historical-Fiction',
-    'History',
+    #'History',
     'Horror',
-    'Humor-and-Comedy',
-    'Manga',
+    #'Humor-and-Comedy',
+    #'Manga',
     'Memoir',
-    'Music',
+    #'Music',
     'Mystery',
-    'Nonfiction',
+    #'Nonfiction',
     'Paranormal',
-    'Philosophy',
+    #'Philosophy',
     'Poetry',
-    'Psychology',
+    #'Psychology',
     #'Religion',
     'Romance',
-    'Science',
-    'Science-Fiction',
+    #'Science',
+    #'Science-Fiction',
     #'Self-Help',
     #'Suspense'
-    'Spirituality',
-    'Sports',
+    #'Spirituality',
+    #'Sports',
     'Thriller',
-    'Travel',
+    #'Travel',
     #'Young-Adult'
     ]
 
-def get_categories():
-
+def get_categories(category_tags=None):
+    if not category_tags:
+        category_tags = _category_tags
+    """ Get most popular category tag for each book. """
     tags = pd.read_csv('./goodreads10k/tags.csv')
     tags_dict = {}
     for idx, row in tags.iterrows():
@@ -56,7 +58,6 @@ def get_categories():
 
     category_book_tags = book_tags[ book_tags.tag_id.isin(c_tags.tag_id)]
     most_popular_tags_idx = (category_book_tags.groupby('goodreads_book_id')['count'].transform(max) == category_book_tags['count'])
-
     for idx, row in category_book_tags[most_popular_tags_idx].iterrows():
         book_categories[row['goodreads_book_id']] = tags_dict[row['tag_id']]
 
